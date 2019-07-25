9bce40ba025fa51ea8bf82623be917644588ff31
Baur Urazalinov <BurhanH@users.noreply.github.com>
2019-07-25 10:54:18 -0500

    Update urls.py

--- djangorest/urls.py
+++ djangorest/urls.py
@@ -25,6 +25,6 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
